dad7948ad14879077f55b35aa04e129be851c503
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2025-01-20 15:26:03 -0300

    Ajuste Carrossel

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+const containerCarrossel = document.getElementById("carrossel_produtos")
+
 // Menu responsivo
 const menuResponsivo = document.getElementById("menu_responsivo")
 let largura = window.innerWidth
@@ -9,9 +11,15 @@ function mudarClasseMenu() {
     if(largura >= 768) {
         menuResponsivo.classList.remove("offcanvas-top")
         menuResponsivo.classList.add("offcanvas-end")
+
+        // Carrossel
+        containerCarrossel.classList.remove("slide")
     } else {
         menuResponsivo.classList.remove("offcanvas-end")
         menuResponsivo.classList.add("offcanvas-top")
+
+        //Carrossel
+        containerCarrossel.classList.add("slide")
     }
 }
 mudarClasseMenu()
@@ -20,13 +28,69 @@ window.addEventListener("resize", () => {
     //console.log("Mudou tamanho")
     largura = window.innerWidth
     mudarClasseMenu()
+
+    // Carrossel
+    larguraCarrossel = document.getElementsByClassName("carousel-inner")[0].clientWidth
+    larguraProduto = document.getElementsByClassName("carousel-item")[0].clientWidth
+    count = (larguraCarrossel / larguraProduto).toFixed(0)
+    scrollPosition = 0
+    carrossel.scrollLeft = 0
 })
 
 
 // Formulario
 const formulario = document.querySelector("#contato>div>form")
-console.log(formulario)
+//console.log(formulario)
 
 formulario.addEventListener("submit", (event) => {
     event.preventDefault()
 })
+
+
+// Carrossel
+let larguraCarrossel = document.getElementsByClassName("carousel-inner")[0].clientWidth
+let larguraProduto = document.getElementsByClassName("carousel-item")[0].clientWidth
+const carrossel = document.getElementsByClassName("carousel-inner")[0]
+const btnNextCarrossel = document.getElementsByClassName("carousel-control-next")[0]
+const btnPrevCarrossel = document.getElementsByClassName("carousel-control-prev")[0]
+let qtdProdutos = document.getElementsByClassName("carousel-item").length
+
+//console.log("Carrossel: " + larguraCarrossel)
+//console.log("Produto: " + larguraProduto)
+//console.log(btnNextCarrossel)
+//console.log(carrossel)
+//console.log(qtdProdutos)
+//console.log("Conta: " + (larguraCarrossel / larguraProduto).toFixed(0))
+
+let count = (larguraCarrossel / larguraProduto).toFixed(0)
+let scrollPosition = 0
+btnNextCarrossel.addEventListener("click", evento => {
+    if(largura >= 768) {
+        //console.log("next")
+        //console.log("Count: " + count)
+        if(count <= qtdProdutos) {
+            scrollPosition += larguraProduto
+            carrossel.scrollLeft = scrollPosition
+            count++
+            //console.log("New Count: " + count)
+        } else {
+            //console.log("max")
+        }
+    }
+})
+
+btnPrevCarrossel.addEventListener("click", evento => {
+    if(largura >= 768) {
+        //console.log("prev")
+        //console.log("Count: " + count)
+        if(scrollPosition > 0) {
+            scrollPosition -= larguraProduto
+            carrossel.scrollLeft = scrollPosition
+            count--
+            //console.log("New Count: " + count)
+        } else {
+            //console.log("min")
+        }
+    }
+})
+
